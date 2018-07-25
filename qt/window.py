88e5fdc198e2b7cf2fb86f42bff0f5cc26bbe727
cbarth15 <39920390+cbarth15@users.noreply.github.com>
2018-07-25 14:53:39 -0400

    Add files via upload
    -Changed Directories
    -Added another completely new window for asking gender preferences
    -Changed picture aspect ratio
    -added a surplus of comments
    Bugs
    -there is a reliance on what.jpg to set the size of the picture window. It should be an easy fix for a later time

--- qt/window.py
+++ qt/window.py
@@ -1,3 +1,7 @@
+'''
+First Window is called and is for choosing sexual preference
+in the button listeners for each of the preferences, a new picture viewing window is made
+'''
 import sys
 import os
 import random
@@ -6,8 +10,57 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5 import QtGui
 import shutil
+#this is for sexual preference selection
+class FirstWindow(QWidget):
 
-
+	def __init__(self):
+		super().__init__()
+		self.title = 'Sexual Preference'
+		self.left = 10
+		self.top = 10
+		self.width = 300
+		self.height = 100
+		self.YesOrNo = None
+		self.initUI()
+		
+	def initUI(self):
+		self.setWindowTitle(self.title)
+		self.setGeometry(self.left, self.top, self.width, self.height)
+		ButtonFemale=QPushButton("Female", self)
+		ButtonFemale.move(50,50)						#reposition the buttons here
+		ButtonFemale.clicked.connect(self.ClickFemale)
+		
+		ButtonBoth=QPushButton("Both", self)
+		ButtonBoth.move(130,50)
+		ButtonBoth.clicked.connect(self.ClickBoth)
+		
+		ButtonMale=QPushButton("Male", self)
+		ButtonMale.move(210,50)
+		ButtonMale.clicked.connect(self.ClickMale)
+		self.show()
+		#action events for each button
+	def ClickFemale(self):
+		global Preference
+		Preference="Female"
+		self.ex=Window()
+		self.hide()
+		self.ex.show()
+		
+	def ClickBoth(self):
+		global Preference
+		Preference="Both"
+		self.ex = Window()
+		self.hide
+		self.ex.show()
+		
+	def ClickMale(self):
+		global Preference
+		Preference="Male"
+		self.ex = Window()
+		self.hide()
+		self.ex.show()
+		
+		#Picture viewing window
 class Window(QWidget):
 
 	def __init__(self):
@@ -25,9 +78,9 @@ class Window(QWidget):
 		self.setGeometry(self.left, self.top, self.width, self.height)
 
 		self.pic = QLabel(self)
-		self.pic.setPixmap(QPixmap("what.jpg"))
-		self.pic.show()
-        
+		self.pic.setPixmap(QPixmap("what.jpg"))		#loads this image just to set the size of the window
+		self.pic.show()								#this image isnt even viewed since it is displayed so briefly
+													#something ill fix sometime
 		buttonYes = QPushButton('Theyre Spicy', self)
 		buttonYes.move(100,400)
 		buttonYes.resize(120, 50)
@@ -40,17 +93,17 @@ class Window(QWidget):
 
 		self.show()
 		self.function()
-		
+		#the main body of the picture selection
 	def function(self):
-		os.chdir("Pictures")		#..//tf_files/catvdog/ is what it should be
-		Preference="Whatever the user chooses as a sexual preference"
-		both=False
+		print(os.getcwd())
+		os.chdir("../tf_files/dataset")		#..//tf_files/catvdog/ is what it should be
+		both=False					#will add in support for both later
 
 		if Preference=="Female":
 			os.chdir("Female")
-		else:                        #Preference=="Male":
+		elif Preference=="Male":                        
 			os.chdir("Male")
-		#else:						need to implement fully
+		else:						#need to implement fully
 			#both=True
 		self.pictures= {}                #new dictionary created
 		counter=0
@@ -61,30 +114,37 @@ class Window(QWidget):
 		
 		counter=0
 		self.length=(len(os.listdir("."))-1)       #get number of files in directory
-		index=random.choice(list(self.pictures))
-		self.temp=self.pictures.pop(index)
-		self.picchange(self.temp)
-		self.length=len(self.pictures)
-			
+													#this is for displaying the first image
+													
+		index=random.choice(list(self.pictures))	#index stores a random key from the pictures dictionary
+		
+		self.temp=self.pictures.pop(index) #delete the image from the dictionary so it doesn't come up again
+											#temp is created which stores filenames
+									
+		self.picchange(self.temp)			#changes the image
+		self.length=len(self.pictures)		#recalculates how many images are now there -1
+		#a function made for displaying images
 	def picchange(self,string):
-		self.pic.setPixmap(QPixmap(string))
-		self.pic.show()
-		self.show()
+		pixmap=QPixmap(string)												#make a picture map from the filename
+		pixmap=pixmap.scaled(self.width,self.height, Qt.KeepAspectRatio)	#scales and resizes the image
+		self.pic.setPixmap(pixmap)											#assign picture mape to qlabel created in init
+		self.pic.show()														#display image
+		self.show()															#update window
 
 	@pyqtSlot()
 	def on_clickYes(self):
-		shutil.move(self.temp,"../Liked/"+self.temp)
-		index=random.choice(list(self.pictures))
-		if len(self.pictures)==0:
+		shutil.move(self.temp,"../catvdog/Like"+self.temp)				#add to liked
+		index=random.choice(list(self.pictures))					#get next image
+		if len(self.pictures)==0:									#if there are no pictures then quit
 			QCoreApplication.quit()
-		self.temp=(self.pictures[index])
-		self.picchange(self.temp)
-		self.pictures.pop(index)
+		self.temp=(self.pictures[index])							#get next image
+		self.picchange(self.temp)									#change to the image
+		self.pictures.pop(index)									#delete the image filename from dictionary
 
 		
 		
 	def on_clickNo(self):
-		shutil.move(self.temp,"../Disliked/"+self.temp)
+		shutil.move(self.temp,"../catvdog/DisLike"+self.temp)
 		index=random.randint(0,self.length)
 		if len(self.pictures)==0:
 			QCoreApplication.quit()
@@ -94,5 +154,5 @@ class Window(QWidget):
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	ex = Window()
+	ex2=FirstWindow()
 	sys.exit(app.exec_())
